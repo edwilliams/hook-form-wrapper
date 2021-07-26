1d9d768cc771da9f41a1d776ba374b20cf05947b
Ed Williams <edwilliamsdevelopment@gmail.com>
2021-07-26 11:24:52 +0100

    added; data to custom event

--- src/form-demo-10/utils.js
+++ src/form-demo-10/utils.js
@@ -1,7 +1,3 @@
-// export const triggerFormChange = ({ data }) => {
-//   document.dispatchEvent(new CustomEvent('cmp-form-onchange', { bubbles: true, detail: { data } }))
-// }
-
-export const triggerFormChange = () => {
-  document.dispatchEvent(new CustomEvent('cmp-form-onchange', { bubbles: true }))
+export const triggerFormChange = detail => {
+  document.dispatchEvent(new CustomEvent('cmp-form-onchange', { bubbles: true, detail }))
 }
